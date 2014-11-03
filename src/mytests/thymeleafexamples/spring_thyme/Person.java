e526f6ab9ef3b2a3caa3aad0a916c80a43a93ab8
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2014-11-03 16:18:53 +0300

    add Person bean

---
+++ src/mytests/thymeleafexamples/spring_thyme/Person.java
@@ -0,0 +1,43 @@
+package mytests.thymeleafexamples.spring_thyme;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * by Irina.Petrovskaya on 11/3/2014.
+ */
+@Component
+public class Person {
+    public Person(String address, int age, String name) {
+        this.address = address;
+        this.age = age;
+        this.name = name;
+    }
+
+    String name;
+    int age;
+    String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
